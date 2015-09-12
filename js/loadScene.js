bc3c09cf16cd731a06381ac49b294e0646c8bf70
Stefan Hedman <schteppe@gmail.com>
2015-09-12 14:21:19 +0200

    updated so it works in chrome

--- js/loadScene.js
+++ js/loadScene.js
@@ -173,12 +173,7 @@
 
 	function initGoo() {
 		// Create typical Goo application.
-		var gooRunner = new goo.GooRunner({
-			antialias: true,
-			manuallyStartGameLoop: true,
-			useDevicePixelRatio: true,
-			logo: false
-		});
+		var gooRunner = new goo.GooRunner({"alpha": false, "useDevicePixelRatio": true, "manuallyStartGameLoop": true, "antialias": true, "logo": false});
 
 		gooRunner.world.add(new goo.AnimationSystem());
 		gooRunner.world.add(new goo.StateMachineSystem(gooRunner));
